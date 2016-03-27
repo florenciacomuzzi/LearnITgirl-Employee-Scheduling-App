0ef75bd84a4dc3f167282a28ed2734271b296bf8
F.Salinas <florenciabsalinas@gmail.com>
2016-03-27 19:35:55 -0400

    format to 12 hr time

---
+++ schedApp/config/initializers/time_formats.rb
@@ -0,0 +1,9 @@
+# config/initializers/time_formats.rb
+
+
+#Time::DATE_FORMATS[:time_12hr] = ->(time) { time.strftime("%I:%M %p") }
+
+#Time::DATE_FORMATS[:short_ordinal]  = ->(time) { time.strftime("%B #{time
+# .day.ordinalize}") }
+
+
